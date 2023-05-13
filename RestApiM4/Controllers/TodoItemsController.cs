511b7f559400d977b014252d0a67905fbe72e6fa
Stranik2504 <58374405+Stranik2504@users.noreply.github.com>
2023-05-13 23:09:45 +0300

    Init

---
+++ RestApiM4/Controllers/TodoItemsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestApiM4.Data;
+using RestApiM4.Models;
+
+namespace RestApiM4.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoItemsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoItems
+        [HttpGet]
+        public async Task<List<TodoItem>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET: api/TodoItems/{id}
+        // GET: api/TodoItems/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(int id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+                return NotFound();
+
+            return todoItem;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+    }
+}
---
+++ RestApiM4/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestApiM4.Models;
+
+namespace RestApiM4.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
